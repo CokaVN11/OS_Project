71036f06b9d56065baa47fb531175c7f6bd20838
KoCa <50935441+CokaVN11@users.noreply.github.com>
2023-03-21 20:58:06 +0700

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,40 +1,47 @@
-
 import customtkinter as ctk
 import tkinter as tk
 from tkinter import ttk
+
 ctk.set_default_color_theme("green")
+
 
 class TreeView(ttk.Treeview):
     def __init__(self, master, **kwargs):
         super().__init__(master)
 
-        self['columns'] = ('Name', 'Size')
-        self.column('#0', width=10)
-        self.column('Name', width=150)
-        self.column('Size', width=150)
+        self["columns"] = "one"
+        self.column("#0", width=200, minwidth=150, stretch=tk.NO)
+        self.column("one", width=100, minwidth=50)
 
-        def MainQuery():
-            files = [(1, '', 'Image'), (2, '', 'Music')]
+        self.heading("#0", text="Name", anchor=tk.W)
+        self.heading("one", text="Type", anchor=tk.W)
+
+        def main_query():
+            """
+            iid, parent_iid, text, values (type)
+            """
+            files = [(1, '', 'Image', []), (2, '', 'Music', [])]
             for file in files:
-                self.insert('',
-                            index=tk.END,
-                            iid=file[0],
-                            text=file[1],
-                            values=file[2])
+                self.insert(parent=file[1],
+                            iid=str(file[0]),
+                            text=file[2],
+                            values=file[3],
+                            index=tk.END)
 
-        def SubQuery():
-            files = [(1, 1, 'Ghe iu dau cua em'), (1, 2, 'Con buom xinh')]
+        def sub_query():
+            files = [(1, 1, 'Ghe iu dau cua em', ['PNG file']), (1, 2, 'Con buom xinh', ['MP3 file'])]
             for file in files:
-                self.insert(parent=file[0],
+                self.insert(parent=str(file[1]),
                             index=tk.END,
-                            iid=f"{file[0]}.{file[1]}",
-                            values=file[2])
+                            iid=f"{file[1]}.{file[0]}",
+                            text=file[2],
+                            values=file[3])
 
-        def Query():
-            MainQuery()
-            SubQuery()
+        def query():
+            main_query()
+            sub_query()
 
-        Query()
+        query()
 
         # self.insert('', tk.END, text='Image', iid=0, tags=1)
         # self.insert('', tk.END, text='Music', iid=1, tags=2)
@@ -45,20 +52,23 @@ class TreeView(ttk.Treeview):
         # self.move(5, 0, 0)
         # self.move(6, 0, 1)
 
+
 class TreeFrame(ctk.CTkScrollableFrame):
-    def __init__(self, master,width,height):
+    def __init__(self, master, width, height):
         super().__init__(master)
         self.configure(width=width,
                        height=height)
         self.tree = TreeView(master=self)
         self.tree.pack(side=ctk.LEFT)
 
+
 class InfoFrame(ctk.CTkFrame):
-    def __init__(self, master, width,height,bg_colour):
+    def __init__(self, master, width, height, bg_colour):
         super().__init__(master)
         self.configure(width=width,
                        height=height,
                        fg_color=bg_colour)
+
 
 # class TabView(ctk.CTkTabview):
 #     def __init__(self, master, width, height, bg_colour):
@@ -85,28 +95,34 @@ class SegmentedButton(ctk.CTkSegmentedButton):
         self.configure(values=["Home", "Menu"])
         self.pack(side=ctk.TOP)
 
+
 class App(ctk.CTk):
     def __init__(self):
         super().__init__()
         self.title("____")
         self.geometry("1080x720")
-        self.grid_rowconfigure(0,weight=1)
+        self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(0, weight=1)
+        self.title("DiskReader")
 
         self.combox = ctk.CTkOptionMenu(master=self,
                                         values=["option1", "option 2"],
                                         width=320,
                                         command=self.optionmenu_callback)
-        self.combox.pack(side=ctk.TOP, anchor=ctk.NW,)
+        # self.combox.pack(side=ctk.TOP, anchor=ctk.NW)
+        self.combox.place(x=0, y=0)
         self.combox.set("option1")
 
         self.left = TreeFrame(master=self,
                               width=300,
                               height=720)
-        self.left.pack(side=ctk.LEFT, pady=(1, 0))
+        # self.left.pack(side=ctk.LEFT, pady=(20, 0))
+        # default height of CTkOptionsMenu = 28
+        self.left.place(x=0, y=28)
 
         self.tab = SegmentedButton(master=self)
-        self.tab.pack(side=ctk.TOP)
+        # self.tab.pack(side=ctk.TOP)
+        self.tab.place(x=300 - 20 + (1080-(300 + 20))/2, y=1)
 
         # self.tabview = TabView(master=self,width=1080-300-20,
         #                       height=720
@@ -114,17 +130,13 @@ class App(ctk.CTk):
         # self.tabview.pack(side=ctk.TOP, ipadx=300)
 
         self.info = InfoFrame(master=self,
-                              width=1080-300-20,
-                              height=720
-                              ,bg_colour="pink")
-        self.info.pack(side=ctk.RIGHT)
-
-
-
-
+                              width=1080 - 300 - 20,
+                              height=720,
+                              bg_colour="pink")
+        # self.info.pack(side=ctk.RIGHT)
+        self.info.place(x= 300 + 20, y=28)
     def optionmenu_callback(self, choice):
         print("Optin menu drop down clicked:", choice)
-
 
 
 app = App()
